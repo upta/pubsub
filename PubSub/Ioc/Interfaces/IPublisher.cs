0955ab3ec0693127a9187f60eae09013b06be6cd
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2022-08-04 22:47:00 +0300

    Updated IoC to follow latest Hub interface.

--- PubSub/Ioc/Interfaces/IPublisher.cs
+++ PubSub/Ioc/Interfaces/IPublisher.cs
@@ -1,7 +1,10 @@
-﻿namespace PubSub
+﻿using System.Threading.Tasks;
+
+namespace PubSub
 {
     public interface IPublisher
     {
         void Publish<T>(T data);
+        Task PublishAsync<T>(T data);
     }
 }
--- PubSub/Ioc/Interfaces/ISubscriber.cs
+++ PubSub/Ioc/Interfaces/ISubscriber.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace PubSub
 {
     public interface ISubscriber
     {
-        bool Exists<T>( object subscriber );
-        bool Exists<T>( object subscriber, Action<T> handler );
-        void Subscribe<T>( object subscriber, Action<T> handler );
-        void Unsubscribe( object subscriber );
-        void Unsubscribe<T>( object subscriber );
-        void Unsubscribe<T>( object subscriber, Action<T> handler );
+        bool Exists<T>();
+        bool Exists<T>(object subscriber);
+        bool Exists<T>(object subscriber, Action<T> handler);
+
+        void Subscribe<T>(Action<T> handler);
+        void Subscribe<T>(object subscriber, Action<T> handler);
+        void Subscribe<T>(Func<T, Task> handler);
+        void Subscribe<T>(object subscriber, Func<T, Task> handler);
+
+        void Unsubscribe();
+        void Unsubscribe(Delegate handler);
+        void Unsubscribe(object subscriber, Delegate handler = null);
+        void Unsubscribe<T>();
+        void Unsubscribe<T>(object subscriber, Delegate handler = null);
     }
 }
